dea383334cd874de05c075b0c30b24a837c2b547
annie940324 <150638771+annie940324@users.noreply.github.com>
2024-06-03 10:19:17 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-// /* MoveNet Skeleton - Steve's Makerspace (most of this code is from TensorFlow)
+/* MoveNet Skeleton - Steve's Makerspace (most of this code is from TensorFlow)
 
 MoveNet is developed by TensorFlow:
 https://www.tensorflow.org/hub/tutorials/movenet
@@ -124,4 +124,3 @@ function drawSkeleton() {
   15 left foot
   16 right foot
 */
-
