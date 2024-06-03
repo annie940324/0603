2c57f506bc231e5ec6189e337cc683f2c3358135
annie940324 <150638771+annie940324@users.noreply.github.com>
2024-06-03 10:08:22 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,129 @@
+// /* MoveNet Skeleton - Steve's Makerspace (most of this code is from TensorFlow)
+
+// MoveNet is developed by TensorFlow:
+// https://www.tensorflow.org/hub/tutorials/movenet
+
+// */
+
+// let video, bodypose, pose, keypoint, detector;
+// let poses = [];
+
+// async function init() {
+//   const detectorConfig = {
+//     modelType: poseDetection.movenet.modelType.MULTIPOSE_LIGHTNING,
+//   };
+//   detector = await poseDetection.createDetector(
+//     poseDetection.SupportedModels.MoveNet,
+//     detectorConfig
+//   );
+// }
+
+// async function videoReady() {
+//   console.log("video ready");
+//   await getPoses();
+// }
+
+// async function getPoses() {
+//   if (detector) {
+//     poses = await detector.estimatePoses(video.elt, {
+//       maxPoses: 2,
+//       //flipHorizontal: true,
+//     });
+//   }
+//   requestAnimationFrame(getPoses);
+// }
+
+// async function setup() {
+//   createCanvas(640, 480);
+//   video = createCapture(VIDEO, videoReady);
+//   video.size(width, height);
+//   video.hide();
+//   await init();
+
+//   stroke(255);
+//   strokeWeight(5);
+// }
+
+// function draw() {
+//   image(video, 0, 0);
+//   drawSkeleton();
+//   // flip horizontal
+//   cam = get();
+//   translate(cam.width, 0);
+//   scale(-1, 1);
+//   image(cam, 0, 0);
+// }
+
+// function drawSkeleton() {
+//   // Draw all the tracked landmark points
+//   for (let i = 0; i < poses.length; i++) {
+//     pose = poses[i];
+//     // shoulder to wrist
+//     for (j = 5; j < 9; j++) {
+//       if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
+//         partA = pose.keypoints[j];
+//         partB = pose.keypoints[j + 2];
+//         line(partA.x, partA.y, partB.x, partB.y);
+//       }
+//     }
+//     // shoulder to shoulder
+//     partA = pose.keypoints[5];
+//     partB = pose.keypoints[6];
+//     if (partA.score > 0.1 && partB.score > 0.1) {
+//       line(partA.x, partA.y, partB.x, partB.y);
+      
+//     }
+//     // hip to hip
+//     partA = pose.keypoints[11];
+//     partB = pose.keypoints[12];
+//     if (partA.score > 0.1 && partB.score > 0.1) {
+//       line(partA.x, partA.y, partB.x, partB.y);
+      
+//     }
+//     // shoulders to hips
+//     partA = pose.keypoints[5];
+//     partB = pose.keypoints[11];
+//     if (partA.score > 0.1 && partB.score > 0.1) {
+//       line(partA.x, partA.y, partB.x, partB.y);
+      
+//     }
+//     partA = pose.keypoints[6];
+//     partB = pose.keypoints[12];
+//     if (partA.score > 0.1 && partB.score > 0.1) {
+//       line(partA.x, partA.y, partB.x, partB.y);
+      
+//     }
+//     // hip to foot
+//     for (j = 11; j < 15; j++) {
+//       if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
+//         partA = pose.keypoints[j];
+//         partB = pose.keypoints[j + 2];
+//         line(partA.x, partA.y, partB.x, partB.y);
+        
+//       }
+//     }
+//   }
+// }
+
+// /* Points (view on left of screen = left part - when mirrored)
+//   0 nose
+//   1 left eye
+//   2 right eye
+//   3 left ear
+//   4 right ear
+//   5 left shoulder
+//   6 right shoulder
+//   7 left elbow
+//   8 right elbow
+//   9 left wrist
+//   10 right wrist
+//   11 left hip
+//   12 right hip
+//   13 left kneee
+//   14 right knee
+//   15 left foot
+//   16 right foot
+// */
 /* MoveNet Skeleton - Steve's Makerspace (most of this code is from TensorFlow)
 
 MoveNet is developed by TensorFlow:
@@ -7,6 +133,11 @@ https://www.tensorflow.org/hub/tutorials/movenet
 
 let video, bodypose, pose, keypoint, detector;
 let poses = [];
+let leftObjectImg, rightObjectImg;
+let studentInfo = {
+  name: "Your Name",
+  studentID: "Your Student ID"
+};
 
 async function init() {
   const detectorConfig = {
@@ -33,12 +164,19 @@ async function getPoses() {
   requestAnimationFrame(getPoses);
 }
 
+async function preload() {
+  // Load your object images
+  leftObjectImg = loadImage('left_object_image.jpg');
+  rightObjectImg = loadImage('right_object_image.jpg');
+}
+
 async function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO, videoReady);
   video.size(width, height);
   video.hide();
   await init();
+  await preload();
 
   stroke(255);
   strokeWeight(5);
@@ -47,11 +185,14 @@ async function setup() {
 function draw() {
   image(video, 0, 0);
   drawSkeleton();
-  // flip horizontal
-  cam = get();
-  translate(cam.width, 0);
-  scale(-1, 1);
-  image(cam, 0, 0);
+  image(leftObjectImg, poses[0].keypoints[1].position.x - 50, poses[0].keypoints[1].position.y - 50, 100, 100); // Left eye
+  image(rightObjectImg, poses[0].keypoints[2].position.x - 50, poses[0].keypoints[2].position.y - 50, 100, 100); // Right eye
+
+  // Draw student info
+  fill(255);
+  textAlign(CENTER);
+  textSize(16);
+  text(studentInfo.name + " " + studentInfo.studentID, width / 2, 20);
 }
 
 function drawSkeleton() {
@@ -71,27 +212,23 @@ function drawSkeleton() {
     partB = pose.keypoints[6];
     if (partA.score > 0.1 && partB.score > 0.1) {
       line(partA.x, partA.y, partB.x, partB.y);
-      
     }
     // hip to hip
     partA = pose.keypoints[11];
     partB = pose.keypoints[12];
     if (partA.score > 0.1 && partB.score > 0.1) {
       line(partA.x, partA.y, partB.x, partB.y);
-      
     }
     // shoulders to hips
     partA = pose.keypoints[5];
     partB = pose.keypoints[11];
     if (partA.score > 0.1 && partB.score > 0.1) {
       line(partA.x, partA.y, partB.x, partB.y);
-      
     }
     partA = pose.keypoints[6];
     partB = pose.keypoints[12];
     if (partA.score > 0.1 && partB.score > 0.1) {
       line(partA.x, partA.y, partB.x, partB.y);
-      
     }
     // hip to foot
     for (j = 11; j < 15; j++) {
@@ -99,28 +236,7 @@ function drawSkeleton() {
         partA = pose.keypoints[j];
         partB = pose.keypoints[j + 2];
         line(partA.x, partA.y, partB.x, partB.y);
-        
       }
     }
   }
 }
-
-/* Points (view on left of screen = left part - when mirrored)
-  0 nose
-  1 left eye
-  2 right eye
-  3 left ear
-  4 right ear
-  5 left shoulder
-  6 right shoulder
-  7 left elbow
-  8 right elbow
-  9 left wrist
-  10 right wrist
-  11 left hip
-  12 right hip
-  13 left kneee
-  14 right knee
-  15 left foot
-  16 right foot
-*/
